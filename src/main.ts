bd2e3f14c052b8f80ea335e94cad3d828087a9fe
lukef3 <lukefoley@outlook.com>
2025-04-20 17:26:51 +0100

    api

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,36 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { Amplify } from "aws-amplify";
 import { parseAmplifyConfig } from "aws-amplify/utils";
+import { fetchAuthSession } from "@aws-amplify/auth";
 import outputs from "../amplify_outputs.json";
 
 const amplifyConfig = parseAmplifyConfig(outputs);
 
-Amplify.configure(outputs);
+const resources = {
+    ...amplifyConfig,
+    API: {
+        REST: {
+            [outputs.custom.api_name]: {
+                endpoint: outputs.custom.api_url,
+                region:   outputs.custom.api_region
+            }
+        }
+    }
+};
+
+const options = {
+    API: {
+        REST: {
+            headers: async () => {
+                const session = await fetchAuthSession();
+                return {
+                    Authorization: session.tokens?.idToken?.toString() ?? ""
+                };
+            }
+        }
+    }
+};
+
+Amplify.configure(resources, options);
 
 createApp(App).mount("#app");
